df03c40b2fbc8b62579c550a4ab7fc711c520da4
BOSTONE069 <bostoneochieng69@gmail.com>
2022-09-29 10:55:57 -0400

    flights management application with django

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
-    path('tasks/', include("tasks.urls"))
+    path('tasks/', include("tasks.urls")),
+    path('flights/', include("flights.urls"))
 ]
