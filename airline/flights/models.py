375893d84fc96058c29a277be183c013adfd1aa4
BOSTONE069 <bostoneochieng69@gmail.com>
2022-09-29 11:32:51 -0400

    flights management application with django updated

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    duration = models.IntegerField()
+
