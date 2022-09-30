9c6afe447808480b82d5460ababd21fc4bc7b45b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-09-30 11:05:33 -0400

    Models used in the airline app has been updated

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=5)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=100)
-    destination = models.CharField(max_length=100)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
